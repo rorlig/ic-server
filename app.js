6d570a3d80c5a090adcccccce1d6d4bf15b711b2
Gaurav Gupta <guptgaurav@gmail.com>
2013-10-16 14:32:04 -0400

    change server

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '123456',
-	database: 'test-ic'
+	database: 'test_ic'
 });
 
 connection.connect();
